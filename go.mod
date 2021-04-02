1a14b66ad1ca198c672bfd51c21a5d34d1051036
Mathisco-01 <mathis.vaneetvelde@gmail.com>
2021-04-03 00:37:14 +0200

    added gh-action build workflow

---
+++ go.mod
@@ -0,0 +1,3 @@
+module git-mirror-action
+
+go 1.16
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 const (
@@ -15,8 +16,8 @@ const (
 
 	UpstreamRemote = "upstream"
 	MirrorRemote   = "mirror"
-	MasterBranch   = "master"
-	NewBranch = "mirror"
+	OriginalBranch = "master"
+	MirrorBranch   = "mirror"
 
 	TempDir = "tmp"
 
@@ -24,18 +25,25 @@ const (
 )
 
 type Config struct {
-	originalURL string
-	mirrorURL   string
-	pat         string
+	originalURL    string
+	originalBranch string
+	mirrorURL      string
+	mirrorBranch   string
+	pat            string
 }
+
+type byteSlice []byte
 
 func main() {
 
 	config := Config{
-		originalURL: ORIGINAL,
-		mirrorURL:   MIRROR,
-		pat:         PAT,
+		originalURL:    ORIGINAL,
+		originalBranch: OriginalBranch,
+		mirrorURL:      MIRROR,
+		mirrorBranch:   MirrorBranch,
+		pat:            PAT,
 	}
+
 
 	err := config.usePAT()
 	if err != nil {
@@ -43,118 +51,128 @@ func main() {
 		return
 	}
 
-	err = gitInit()
+	// init
+	out, err := gitInit()
 	if err != nil {
 		log.Println(err)
+		log.Printf("Output: %v\n", out)
 		return
 	}
 
-	err = addRemote(UpstreamRemote, config.originalURL)
+	// add upstream remote (url of repo we want to clone)
+	out, err = addRemote(UpstreamRemote, config.originalURL)
 	if err != nil {
 		log.Println(err)
+		log.Printf("Output: %v\n", out)
 		return
 	}
 
-	err = fetch(UpstreamRemote)
+	// add mirror remote (url of repo we want to mirror to)
+	out, err = addRemote(MirrorRemote, config.mirrorURL)
 	if err != nil {
 		log.Println(err)
+		log.Printf("Output: %v\n", out)
 		return
 	}
 
-	err = addRemote(MirrorRemote, config.mirrorURL)
+	// checks out branch on upstream remote
+	out, err = checkout(UpstreamRemote, config.originalBranch)
 	if err != nil {
 		log.Println(err)
+		log.Printf("Output: %v\n", out)
 		return
 	}
 
-	err = fetch(MirrorRemote)
+	// pulls branch on upstream remote
+	out, err = pull(UpstreamRemote, config.originalBranch)
 	if err != nil {
 		log.Println(err)
+		log.Printf("Output: %v\n", out)
 		return
 	}
 
-	err = checkout(UpstreamRemote, MasterBranch)
+	// makes new branch
+	out, err = branch(config.mirrorBranch)
 	if err != nil {
 		log.Println(err)
+		log.Printf("Output: %v\n", out)
 		return
 	}
 
-	err = pull(UpstreamRemote, MasterBranch)
+	// pushes new branch to mirror remote
+	out, err = push(MirrorRemote, config.mirrorBranch)
 	if err != nil {
 		log.Println(err)
-		return
-	}
-
-	err = branch(NewBranch)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	err = push(MirrorRemote, NewBranch)
-	if err != nil {
-		log.Println(err)
+		log.Printf("Output: %v\n", out)
 	}
 }
 
-func gitInit() error {
+// initialize git repository
+func gitInit() (output string, err error) {
 	log.Printf("initializing git")
 	return command("init")
 }
 
-func addRemote(name string, repo string) error {
+// adds new remote to local git repository
+// also fetches branches from the repository (--fetch flag at the end)
+func addRemote(name string, repo string) (output string, err error) {
 	log.Printf("adding remote: %v\n", name)
-	return command("remote", "add", name, repo)
+	return command("remote", "add", name, repo, "--fetch")
 }
 
-func fetch(remote string) error {
-	log.Printf("fetching remote: %v\n", remote)
-	return command("fetch", remote)
-}
-
-func checkout(remote string, branch string) error {
+// checks out specific branch from specific remote in local git repository
+func checkout(remote string, branch string) (output string, err error) {
 	log.Printf("checking out: %v/%v\n", remote, branch)
 	return command("checkout", fmt.Sprintf("%v/%v", remote, branch))
 }
 
-func pull(remote string, branch string) error {
+// pulls specific branch from specific remote
+func pull(remote string, branch string) (output string, err error) {
 	log.Printf("pulling: %v/%v", remote, branch)
 	return command("pull", remote, branch)
 }
 
-func push(remote string, branch string) error {
+// pushes to specific branch on remote
+func push(remote string, branch string) (output string, err error) {
 	log.Printf("pushing: %v/%v\n", remote, branch)
-	return command("push","--set-upstream", remote, branch)
+	return command("push", "--set-upstream", remote, branch, "--force")
 }
 
-func branch(name string) error {
+// creates a new branch with specific name
+func branch(name string) (output string, err error) {
 	log.Printf("creating branch: %v", name)
 	return command("branch", name)
 }
 
-func command(args ...string) error {
+// executes git commands in the TempDir folder
+func command(args ...string) (out string, err error) {
 	cwd, _ := os.Getwd()
 	pathArgs := []string{"-C", fmt.Sprintf("%v/%v", cwd, TempDir)}
 	args = append(pathArgs, args...)
 
 	cmd := exec.Command("git", args...)
 
+	// makes sure the TempDir exist
 	if _, err := os.Stat(fmt.Sprintf("%v/%v", cwd, TempDir)); os.IsNotExist(err) {
 		err := os.Mkdir(TempDir, 0777)
 		if err != nil {
-			return err
+			return "", err
 		}
 	}
 
-	err := cmd.Start()
+	// starts command
+	var output byteSlice
+	output, err = cmd.Output()
 	if err != nil {
 		log.Println(err)
-		return err
+		return "", err
 	}
 
-	err = cmd.Wait()
-	return err
+	out = output.byteSliceToString()
+	return out, err
 }
 
+// converts url in Config type to use Personal Access Token
 func (c *Config) usePAT() (err error) {
 	c.originalURL, err = convertURL(c.originalURL, c.pat)
 	if err != nil {
@@ -165,10 +183,21 @@ func (c *Config) usePAT() (err error) {
 	return err
 }
 
+// adds pat to https url
 func convertURL(url string, pat string) (s string, err error) {
 	if url[:8] != "https://" {
 		return "", errors.New(ErrUrlNotHttps)
 	}
 
-	return fmt.Sprintf("https://git-mirror-action:%v@%v", pat, url[8:]), err
+	return fmt.Sprintf("https://%v@%v", pat, url[8:]), err
+}
+
+// convert a byteSlice to a string
+func (b byteSlice) byteSliceToString() string {
+	var a []string
+	for _, byte := range b {
+		a = append(a, string(byte))
+	}
+
+	return strings.Join(a, " ")
 }
