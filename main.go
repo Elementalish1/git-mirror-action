374f4828021966e3a927070d1ba12aff19e7d6d8
Mathisco-01 <mathis.vaneetvelde@gmail.com>
2021-04-03 14:14:32 +0200

    added githubaction code

--- main.go
+++ main.go
@@ -1,58 +1,127 @@
 package main
 
 import (
+	"encoding/base64"
 	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
+
+	"github.com/sethvargo/go-githubactions"
 )
 
 const (
-	ORIGINAL = ""
-	MIRROR   = ""
-	PAT      = ""
-
+	// name of remote urls
 	UpstreamRemote = "upstream"
 	MirrorRemote   = "mirror"
-	OriginalBranch = "master"
-	MirrorBranch   = "mirror"
 
-	TempDir = "tmp"
+	// name used as temporary directory
+	TempDir = "/tmp/"
 
-	ErrUrlNotHttps = "url is not https"
+	// error messages
+	ErrURLNotHTTPS   = "url is not https"
+	ErrNoOriginalURL = "no original repository url provided"
+	ErrNoMirrorURL   = "no mirror repository url provided"
+	ErrNoPAT         = "no personal access token provided"
+	ErrFailedToBase64DecodePAT
+
+	// info messages
+	InfoNoOriginalBranch = "no original branch provided, using 'master'"
+	InfoNoMirrorBranch   = "no mirror branch provided, using 'mirror'"
+	InfoUsingForce = "git will now use --force to push"
 )
 
-type Config struct {
+// config struct which holds all information required
+type config struct {
 	originalURL    string
 	originalBranch string
 	mirrorURL      string
 	mirrorBranch   string
 	pat            string
+	useForce		bool
 }
 
 type byteSlice []byte
 
 func main() {
 
-	config := Config{
-		originalURL:    ORIGINAL,
-		originalBranch: OriginalBranch,
-		mirrorURL:      MIRROR,
-		mirrorBranch:   MirrorBranch,
-		pat:            PAT,
+	// get originalURL input (required)
+	originalURL := githubactions.GetInput("originalURL")
+	if originalURL == "" {
+		githubactions.Fatalf(ErrNoOriginalURL)
+		return
 	}
 
+	// get originalBranch input (optional)
+	originalBranch := githubactions.GetInput("originalBranch")
+	if originalBranch == "" {
+		log.Println(InfoNoOriginalBranch)
+		originalBranch = "master"
+	}
 
-	err := config.usePAT()
+	// get mirrorURL input (required)
+	mirrorURL := githubactions.GetInput("originalURL")
+	if mirrorURL == "" {
+		githubactions.Fatalf(ErrNoMirrorURL)
+		return
+	}
+
+	// get mirrorBranch input (optional)
+	mirrorBranch := githubactions.GetInput("originalBranch")
+	if mirrorBranch == "" {
+		log.Println(InfoNoMirrorBranch)
+		mirrorBranch = "master"
+	}
+
+	// get Personal Access Token encoded in base64 input (required)
+	patEncoded := githubactions.GetInput("PAT")
+	if patEncoded == "" {
+		githubactions.Fatalf(ErrNoPAT)
+		return
+	}
+
+	// add encoded PAT to mask to make sure it doesn't get logged to console
+	githubactions.AddMask(patEncoded)
+
+	// base64 decode PAT
+	pat, err := base64.StdEncoding.DecodeString(patEncoded)
+	if err != nil {
+		githubactions.Fatalf(ErrFailedToBase64DecodePAT)
+		return
+	}
+
+	// add true PAT to mask
+	githubactions.AddMask(string(pat))
+
+	// get useForce input to see if push can use the argument `--force`
+	var useForce = false
+	useForceInput := githubactions.GetInput("useForce")
+	if useForceInput == "yes" {
+		log.Println(InfoUsingForce)
+		useForce = true
+	}
+
+	// make config
+	config := config{
+		originalURL:    originalURL,
+		originalBranch: originalBranch,
+		mirrorURL:      mirrorURL,
+		mirrorBranch:   mirrorBranch,
+		pat:            string(pat),
+		useForce: useForce,
+	}
+
+	// convert URLs to use PAT
+	err = config.usePAT()
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	// init
-	out, err := gitInit()
+	// init git repository
+	out, err := config.gitInit()
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -60,7 +129,7 @@ func main() {
 	}
 
 	// add upstream remote (url of repo we want to clone)
-	out, err = addRemote(UpstreamRemote, config.originalURL)
+	out, err = config.addRemote(UpstreamRemote, config.originalURL)
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -68,7 +137,7 @@ func main() {
 	}
 
 	// add mirror remote (url of repo we want to mirror to)
-	out, err = addRemote(MirrorRemote, config.mirrorURL)
+	out, err = config.addRemote(MirrorRemote, config.mirrorURL)
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -76,7 +145,7 @@ func main() {
 	}
 
 	// checks out branch on upstream remote
-	out, err = checkout(UpstreamRemote, config.originalBranch)
+	out, err = config.checkout(UpstreamRemote, config.originalBranch)
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -84,7 +153,7 @@ func main() {
 	}
 
 	// pulls branch on upstream remote
-	out, err = pull(UpstreamRemote, config.originalBranch)
+	out, err = config.pull(UpstreamRemote, config.originalBranch)
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -92,7 +161,7 @@ func main() {
 	}
 
 	// makes new branch
-	out, err = branch(config.mirrorBranch)
+	out, err = config.branch(config.mirrorBranch)
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -100,7 +169,7 @@ func main() {
 	}
 
 	// pushes new branch to mirror remote
-	out, err = push(MirrorRemote, config.mirrorBranch)
+	out, err = config.push(MirrorRemote, config.mirrorBranch)
 	if err != nil {
 		log.Println(err)
 		log.Printf("Output: %v\n", out)
@@ -108,52 +177,56 @@ func main() {
 }
 
 // initialize git repository
-func gitInit() (output string, err error) {
+func (c *config) gitInit() (output string, err error) {
 	log.Printf("initializing git")
 	return command("init")
 }
 
 // adds new remote to local git repository
 // also fetches branches from the repository (--fetch flag at the end)
-func addRemote(name string, repo string) (output string, err error) {
+func (c *config) addRemote(name string, repo string) (output string, err error) {
 	log.Printf("adding remote: %v\n", name)
 	return command("remote", "add", name, repo, "--fetch")
 }
 
 // checks out specific branch from specific remote in local git repository
-func checkout(remote string, branch string) (output string, err error) {
+func (c *config) checkout(remote string, branch string) (output string, err error) {
 	log.Printf("checking out: %v/%v\n", remote, branch)
 	return command("checkout", fmt.Sprintf("%v/%v", remote, branch))
 }
 
 // pulls specific branch from specific remote
-func pull(remote string, branch string) (output string, err error) {
+func (c *config)pull(remote string, branch string) (output string, err error) {
 	log.Printf("pulling: %v/%v", remote, branch)
 	return command("pull", remote, branch)
 }
 
 // pushes to specific branch on remote
-func push(remote string, branch string) (output string, err error) {
+func (c *config) push(remote string, branch string) (output string, err error) {
 	log.Printf("pushing: %v/%v\n", remote, branch)
-	return command("push", "--set-upstream", remote, branch, "--force")
+	if c.useForce {
+		return command("push", "--set-upstream", remote, branch, "--force")
+	}
+
+	return command("push", "--set-upstream", remote, branch)
 }
 
 // creates a new branch with specific name
-func branch(name string) (output string, err error) {
+func (c *config) branch(name string) (output string, err error) {
 	log.Printf("creating branch: %v", name)
 	return command("branch", name)
 }
 
 // executes git commands in the TempDir folder
 func command(args ...string) (out string, err error) {
-	cwd, _ := os.Getwd()
-	pathArgs := []string{"-C", fmt.Sprintf("%v/%v", cwd, TempDir)}
+
+	pathArgs := []string{"-C", TempDir}
 	args = append(pathArgs, args...)
 
 	cmd := exec.Command("git", args...)
 
 	// makes sure the TempDir exist
-	if _, err := os.Stat(fmt.Sprintf("%v/%v", cwd, TempDir)); os.IsNotExist(err) {
+	if _, err := os.Stat(TempDir); os.IsNotExist(err) {
 		err := os.Mkdir(TempDir, 0777)
 		if err != nil {
 			return "", err
@@ -168,12 +241,12 @@ func command(args ...string) (out string, err error) {
 		return "", err
 	}
 
-	out = output.byteSliceToString()
+	out = output.ToString()
 	return out, err
 }
 
 // converts url in Config type to use Personal Access Token
-func (c *Config) usePAT() (err error) {
+func (c *config) usePAT() (err error) {
 	c.originalURL, err = convertURL(c.originalURL, c.pat)
 	if err != nil {
 		return err
@@ -186,17 +259,17 @@ func (c *Config) usePAT() (err error) {
 // adds pat to https url
 func convertURL(url string, pat string) (s string, err error) {
 	if url[:8] != "https://" {
-		return "", errors.New(ErrUrlNotHttps)
+		return "", errors.New(ErrURLNotHTTPS)
 	}
 
 	return fmt.Sprintf("https://%v@%v", pat, url[8:]), err
 }
 
 // convert a byteSlice to a string
-func (b byteSlice) byteSliceToString() string {
+func (b byteSlice) ToString() string {
 	var a []string
-	for _, byte := range b {
-		a = append(a, string(byte))
+	for _, by := range b {
+		a = append(a, string(by))
 	}
 
 	return strings.Join(a, " ")
